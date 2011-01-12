9d195d05ad6b1c3a9fbc80ee6b5b65838da6d1a7
Ilya <ilya@hrsandbox.org>
2011-01-13 00:03:27 +0500

    Задача про objcopy

--- 08. ObjCopy/main.asm
+++
@@ -1,7 +0,0 @@
-;BITS 32 ; “ут
-global _start
-;section .text ;
-_start:
-	mov		eax,1
-    int		0x80
-    ret
--- 08. ObjCopy/test.c
+++ 08. ObjCopy/test.c
@@ -1,6 +1,8 @@
-int sum(int a, int b){
-	return a+b;
-}
+static int a[1000][1000];
+static int b[] = { 1, 2, 3, 4, 5,6,7 };
+static int c = 12345678;
+
 int main(){
+	
 	return 0;
 }
